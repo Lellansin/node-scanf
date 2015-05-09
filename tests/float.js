f92bd2950894e28d6ce35df8e3ea90d87160bb9c
Lellansin <lellansin@gmail.com>
2015-05-09 12:54:21 +0800

    negative number support

--- tests/float.js
+++ tests/float.js
@@ -10,6 +10,12 @@ describe('scanf', function() {
       done();
     });
 
+    it('[%f] \t\tshould get a float number -3.14', function(done) {
+      var num = sscanf('-3.14', '%f');
+      should.strictEqual(num, -3.14);
+      done();
+    });
+
     it('[%f] \t\tshould get a float number 3', function(done) {
       var num = sscanf('3', '%f');
       should.strictEqual(num, 3);
@@ -19,6 +25,12 @@ describe('scanf', function() {
     it('[a=%f] \t\tshould get a float number 3.14', function(done) {
       var num = sscanf('a=3.14 456', 'a=%f');
       should.strictEqual(num, 3.14);
+      done();
+    });
+
+    it('[a=%f] \t\tshould get a float number -3.14', function(done) {
+      var num = sscanf('a=-3.14 456', 'a=%f');
+      should.strictEqual(num, -3.14);
       done();
     });
 
@@ -34,15 +46,33 @@ describe('scanf', function() {
       done();
     });
 
+    it('[%f,%f] \tshould get an array with two float [2.5, -3.6]', function(done) {
+      var num = sscanf('2.5,-3.6', '%f,%f');
+      should.deepEqual(num, [2.5, -3.6]);
+      done();
+    });
+
     it('[a=%f, b=%f] \tshould get an array with two float [2.5, 3.6]', function(done) {
       var num = sscanf('a=2.5, b=3.6', 'a=%f, b=%f');
       should.deepEqual(num, [2.5, 3.6]);
       done();
     });
 
+    it('[a=%f, b=%f] \tshould get an array with two float [2.5, -3.6]', function(done) {
+      var num = sscanf('a=2.5, b=-3.6', 'a=%f, b=%f');
+      should.deepEqual(num, [2.5, -3.6]);
+      done();
+    });
+
     it('[%d%d%d a=%f, b=%f] \tshould get an array with two float [2.5, 3.6]', function(done) {
       var num = sscanf('1 2 3 a=2.5, b=3.6 8', '%d%d%d a=%f, b=%f %d');
       should.deepEqual(num, [ 1, 2, 3, 2.5, 3.6, 8 ]);
+      done();
+    });
+
+    it('[%d%d%d a=%f, b=%f] \tshould get an array with two float [2.5, -3.6]', function(done) {
+      var num = sscanf('1 2 3 a=2.5, b=-3.6 8', '%d%d%d a=%f, b=%f %d');
+      should.deepEqual(num, [ 1, 2, 3, 2.5, -3.6, 8 ]);
       done();
     });
   });
--- tests/int.js
+++ tests/int.js
@@ -10,15 +10,33 @@ describe('scanf', function() {
       done();
     });
 
+    it('[%d] \t\tshould get a integer number -123', function(done) {
+      var num = sscanf('-123', '%d');
+      should.strictEqual(num, -123);
+      done();
+    });
+
     it('[%d] \t\tshould get a integer number 3', function(done) {
       var num = sscanf('3.14', '%d');
       should.strictEqual(num, 3);
       done();
     });
 
+    it('[%d] \t\tshould get a integer number -3', function(done) {
+      var num = sscanf('-3.14', '%d');
+      should.strictEqual(num, -3);
+      done();
+    });
+
     it('[%d%d] \t\tshould get [NaN, 3]', function(done) {
       var num = sscanf('a 3.14', '%d%d');
       should.deepEqual(num, [NaN, 3]);
+      done();
+    });
+
+    it('[%d%d] \t\tshould get [NaN, -3]', function(done) {
+      var num = sscanf('a -3.14', '%d%d');
+      should.deepEqual(num, [NaN, -3]);
       done();
     });
 
@@ -40,6 +58,12 @@ describe('scanf', function() {
       done();
     });
 
+    it('[a=%d] \t\tshould get a integer number -123', function(done) {
+      var num = sscanf('a=-123 456', 'a=%d');
+      should.strictEqual(num, -123);
+      done();
+    });
+
     it('[hex=%d] \tshould get a integer number from hex', function(done) {
       var num = sscanf('hex=0x10', 'hex=%d');
       should.strictEqual(num, 16);
@@ -55,6 +79,12 @@ describe('scanf', function() {
     it('[%d%d] \t\tshould get an array with two integers [5, 6]', function(done) {
       var num = sscanf('5,6', '%d%d');
       should.deepEqual(num, [5, 6]);
+      done();
+    });
+
+    it('[%d%d] \t\tshould get an array with two integers [5, -6]', function(done) {
+      var num = sscanf('5,-6', '%d%d');
+      should.deepEqual(num, [5, -6]);
       done();
     });
 
@@ -76,6 +106,12 @@ describe('scanf', function() {
       done();
     });
 
+    it('[%d,%d] \tshould get an array with two integers [-5, null]', function(done) {
+      var num = sscanf('-5,', '%d,%d');
+      should.deepEqual(num, [-5, null]);
+      done();
+    });
+
     it('[a=%d, b=%d] \tshould get an array with two integer [5, 6]', function(done) {
       var num = sscanf('a=5, b=6', 'a=%d, b=%d');
       should.deepEqual(num, [5, 6]);
@@ -85,6 +121,12 @@ describe('scanf', function() {
     it('[a=%d, b=%d] \tshould get an array with two integer [5, null]', function(done) {
       var num = sscanf('a=5', 'a=%d, b=%d');
       should.deepEqual(num, [5, null]);
+      done();
+    });
+
+    it('[a=%d, b=%d] \tshould get an array with two integer [-5, null]', function(done) {
+      var num = sscanf('a=-5', 'a=%d, b=%d');
+      should.deepEqual(num, [-5, null]);
       done();
     });
   });
