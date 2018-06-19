ac407d578892ade99deea2af19bdf858d1453a47
黄鼎恒 <huangdh@tuya.com>
2018-06-19 10:54:19 +0800

    feat: support for special CN character situation

--- test/str.js
+++ test/str.js
@@ -10,6 +10,13 @@ describe('scanf', function() {
       done();
     });
 
+    // Not supported now
+    // it('[%s] \t\tshould get a string ф', function(done) {
+    //   var str = sscanf('ф', '%s');
+    //   should.strictEqual(str, 'ф');
+    //   done();
+    // });
+
     it('[%s] \t\tshould get a string 3.14', function(done) {
       var str = sscanf('3.14 is pi', '%s');
       should.strictEqual(str, '3.14');
@@ -73,6 +80,18 @@ describe('scanf', function() {
     it('[%s, %s] \tshould get an array like ["Alan", "6"]', function(done) {
       var str = sscanf('Alan, 6', '%s,%s');
       should.deepEqual(str, ['Alan', '6']);
+      done();
+    });
+
+    it('[%s %f %f] \tshould get an array like ["Alan", 1.1, 2]', function(done) {
+      var str = sscanf('Alan 1.1 2', '%s %f %f');
+      should.deepEqual(str, ['Alan', 1.1, 2]);
+      done();
+    });
+
+    it('[%s %f %f] \tshould get an array like ["兴安盟", 122.05, 46.08]', function(done) {
+      let str = sscanf('兴安盟 122.05 46.08', '%s %f %f')
+      should.deepEqual(str, ['兴安盟', 122.05, 46.08]);
       done();
     });
 
