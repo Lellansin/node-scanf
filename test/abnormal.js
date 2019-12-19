ed59bf3898d1380bdd4e60f6bce181016e90a0c8
冰森 <dingheng.hdh@alibaba-inc.com>
2019-12-20 00:04:01 +0800

    test: add test case for non-selector

--- test/abnormal.js
+++ test/abnormal.js
@@ -1,8 +1,19 @@
+var scanf = require('../');
 var sscanf = require('../').sscanf;
 var should = require('should');
 
 describe('scanf', function() {
   describe('#invalidator', function() {
+    it('should get error while scanf undeinfed string', function() {
+      try {
+        scanf('', '');
+      } catch (err) {
+        should.strictEqual(err.message, 'Unable to parse scanf selector.');
+        return;
+      }
+      throw new Error('it could not come here');
+    });
+
     it('should get null while sscanf undeinfed string', function() {
       const res = sscanf(undefined);
       should.strictEqual(res, null);
