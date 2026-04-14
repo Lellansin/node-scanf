17960412e6a9671d9dd9db5f5676b19a87c180d8
Lellansin Huang <Lellansin@gmail.com>
2026-04-14 09:33:51 +0800

    test: Improve coverage (#47)
    * Improve coverage
    
    * Make gets dependency injectable for testing

---
+++ test/gets.js
@@ -0,0 +1,63 @@
+var should = require('should');
+
+var getsPath = require.resolve('../lib/gets');
+var fs = require('fs');
+
+describe('gets', function() {
+  afterEach(function() {
+    delete require.cache[getsPath];
+  });
+
+  function loadGetsWith(stubs) {
+    delete require.cache[getsPath];
+    var getsModule = require('../lib/gets');
+
+    var localFs = Object.assign({}, fs, stubs);
+    return getsModule.withFs(localFs);
+  }
+
+  it('should return empty string when no bytes are read', function() {
+    var gets = loadGetsWith({
+      openSync: function() {
+        return 0;
+      },
+      readSync: function() {
+        return 0;
+      }
+    });
+
+    var result = gets();
+    should.strictEqual(result, '');
+  });
+
+  it('should return empty string when EOF is reached', function() {
+    var gets = loadGetsWith({
+      openSync: function() {
+        return 0;
+      },
+      readSync: function() {
+        var err = new Error('EOF');
+        err.code = 'EOF';
+        throw err;
+      }
+    });
+
+    var result = gets();
+    should.strictEqual(result, '');
+  });
+
+  it('should read and trim newline-terminated input', function() {
+    var gets = loadGetsWith({
+      openSync: function() {
+        return 0;
+      },
+      readSync: function(fd, buffer) {
+        buffer.write('hello\n');
+        return 6;
+      }
+    });
+
+    var result = gets();
+    should.strictEqual(result, 'hello');
+  });
+});
